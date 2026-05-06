b008b5075b3412d3a43a50042091795669f9526e
jayantrao207-commits <jayantrao207@gmail.com>
2026-05-06 11:35:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,6 +207,15 @@ with c1:
     """, unsafe_allow_html=True)
 
 with c2:
+    if len(f) > 0:
+
+    top_area = f['Area'].value_counts().idxmax()
+    top_crime = f['Crime_Type'].value_counts().idxmax()
+
+else:
+
+    top_area = "No Data"
+    top_crime = "No Data"
 
     top_area = f['Area'].value_counts().idxmax()
 
